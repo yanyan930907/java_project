efea9bb9919a4f1f8f07f471b5aa20d944c1e24a
hugo0329 <hugo940329@gmail.com>
2025-05-22 17:36:16 +0800

    重傳

--- java_final_project/Card.java
+++ java_final_project/Card.java
@@ -7,20 +7,31 @@ public class Card {
     private ImageIcon image;
     private String backHint;  // 背面提示
     private String category;  // 分類，例如：「錯題」
+    private String linkedFilePath;  // 關聯檔案的路徑或URL
 
-    public Card(String frontText, ImageIcon image, String backHint, String category) {
+
+    public Card(String frontText, ImageIcon image, String backHint, String category, String linkedFilePath) {
         this.frontText = frontText;
         this.image = image;
         this.backHint = backHint;
         this.category = category;
+        this.linkedFilePath = linkedFilePath;
     }
+
+    public void setFrontText(String text){ this.frontText = text; }
+    public void setImage(ImageIcon image) { this.image = image; }
+    public void setBackHint(String hint) { this.backHint = hint; }
+    public void setCategory(String category) { this.category = category; }
+    public void setLinkedFilePath(String linkedFilePath) { this.linkedFilePath = linkedFilePath; }
 
     public String getFrontText() { return frontText; }
     public ImageIcon getImage() { return image; }
     public String getBackHint() { return backHint; }
     public String getCategory() { return category; }
+    public String getLinkedFilePath() {return linkedFilePath; }
 
     public void display(){  // 顯示卡片內容
         
-    } 
+    }
+    
 }
